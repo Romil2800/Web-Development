ae0f091403a557e1984743047ce457a0a881ba63
Romilkumar Patel <68607710+Romil2800@users.noreply.github.com>
2022-08-30 18:23:21 -0300

    updated_data

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.hamburger').on('click', function () {
+        $('.sidebar').toggleClass('active');
+    });
+     $('.cancel').on('click', function () {
+        $('.sidebar').toggleClass('active');
+    });
+});
